239a1e49ff8521a1137e55d640af68f95d8f23d7
Kleber Soares <kleberr@msn.com>
2014-09-25 20:30:43 -0300

    #4411434 - Envio de email #1

--- canaa/core/forms.py
+++ canaa/core/forms.py
@@ -29,7 +29,8 @@ class ContactForm(forms.Form):
         message_html = render_to_string('contact_mail.html', context)
         msg = EmailMultiAlternatives(subject, message,
                                      'no-reply@canaa.ind.br',
-                                     'contato@canaa.ind.br',)
+                                     'kleberr@msn.com')
+                                     # 'contato@canaa.ind.br',)
 
         msg.attach_alternative(message_html, 'text/html')
         msg.send()
